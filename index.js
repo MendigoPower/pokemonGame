d625c0c7befafa4dfeca26ee160b59292c99443b
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-04-05 23:47:06 -0700

    collisions file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1360;
-canvas.height = 760;
+
+// Adding the Map at the screen //  
+
+canvas.width = 1366;
+canvas.height = 768;
 
 c.fillStyle = 'grey'
 c.fillRect(0, 0, canvas.width, canvas.height);
@@ -10,10 +13,106 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = './img/rainCouverTown.png';
 
+
+// Adding the Player at the screen //  
+
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
-image.onload = () => {
-    c.drawImage(image, -285, -1100);
-    c.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2, canvas.height / 2 - playerImage.height / 2);
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
 }
+
+const background = new Sprite({
+    position: {
+        x: -278,
+        y: -1140
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4 / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height,
+    )
+
+    if (keys.w.pressed && lastKey == 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey == 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey == 's') background.position.y -= 3
+    else if (keys.d.pressed && lastKey == 'd') background.position.x -= 3
+
+}
+animate()
+
+
+// Moving the player through map //  
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+})
