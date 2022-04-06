1b710db5a38b0bb74b5d7938b5e8f8cca5ea69e1
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-04-05 19:34:39 -0700

    size adjust

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1360;
+canvas.height = 760;
 
 c.fillStyle = 'grey'
 c.fillRect(0, 0, canvas.width, canvas.height);
@@ -14,6 +14,6 @@ const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
 image.onload = () => {
-    c.drawImage(image, -290, -1100);
+    c.drawImage(image, -285, -1100);
     c.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2, canvas.height / 2 - playerImage.height / 2);
 }
