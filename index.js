df69c83603ac1432b4fdc442e2d9fb914c06b2b6
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-04-07 00:28:44 -0700

    battle screen add

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ const player = new Sprite({
     },
     image: playerDownImage,
     frames: {
-        max: 4
+        max: 4,
+        hold: 20
     },
     sprites: {
         up: playerUpImage,
@@ -150,7 +151,7 @@ function animate() {
     foreground.draw()
 
     let moving = true
-    player.moving = false
+    player.animate = false
 
     if (battleZones.initiated) return
     // activate battle
@@ -201,7 +202,7 @@ function animate() {
 
 
     if (keys.w.pressed && lastKey == 'w') {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.up
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
@@ -227,7 +228,7 @@ function animate() {
                 movable.position.y += 3
             })
     } else if (keys.a.pressed && lastKey == 'a') {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.left
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
@@ -253,7 +254,7 @@ function animate() {
             })
 
     } else if (keys.s.pressed && lastKey == 's') {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.down
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
@@ -278,7 +279,7 @@ function animate() {
                 movable.position.y -= 3
             })
     } else if (keys.d.pressed && lastKey == 'd') {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.right
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i]
@@ -304,7 +305,7 @@ function animate() {
             })
     }
 }
-animate()
+// animate()
 
 const battleBackgroundImage = new Image()
 battleBackgroundImage.src = './img/battleBackground.png'
@@ -315,10 +316,48 @@ const battleBackground = new Sprite({
     },
     image: battleBackgroundImage
 })
+
+const draggleImage = new Image()
+draggleImage.src = './img/draggleSprite.png'
+const draggle = new Sprite ({
+    position: {
+        x: 1070,
+        y: 150,
+    },
+    image: draggleImage,
+    frames: {
+        max: 4,
+        hold: 60
+    },
+    animate: true
+})
+
+const embyImage = new Image()
+embyImage.src = './img/embySprite.png'
+const emby = new Sprite ({
+    position: {
+        x: 360,
+        y: 400,
+    },
+    image: embyImage,
+    frames: {
+        max: 4,
+        hold: 60
+    },
+    animate: true
+})
+
 function animateBattle() {
     window.requestAnimationFrame(animateBattle)
     battleBackground.draw()
+    draggle.draw()
+    emby.draw()
 }
+
+animateBattle()
+// animate()
+
+
 
 // Moving the player through map //  
 
