1cd366efa96e189851f97b3669319bba51017a04
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-04-07 14:18:39 -0700

    transition back to map added

--- data/classes.js
+++ data/classes.js
@@ -8,12 +8,14 @@ class Sprite {
         rotation = 0,
     }) {
         this.position = position
-        this.image = image
+        this.image = new Image
         this.frames = { ...frames, val: 0, elapsed: 0 }
         this.image.onload = () => {
             this.width = this.image.width / this.frames.max
             this.height = this.image.height / this.frames.max
         }
+        this.image.src = image.src
+
         this.animate = animate
         this.sprites = sprites
         this.opacity = 1
--- data/monsters.js
+++ data/monsters.js
@@ -1,37 +1,35 @@
-const embyImage = new Image()
-embyImage.src = './img/embySprite.png'
-
-const draggleImage = new Image()
-draggleImage.src = './img/draggleSprite.png'
-
 const monsters = {
     Emby: {
-            position: {
-                x: 360,
-                y: 400,
-            },
-            image: embyImage,
-            frames: {
-                max: 4,
-                hold: 60
-            },
-            animate: true,
-            name:'Emby',
-            attacks: [attacks.Tackle, attacks.Fireball]
+        position: {
+            x: 360,
+            y: 400,
         },
-        Draggle: {
-            position: {
-                x: 1070,
-                y: 150,
-            },
-            image: draggleImage,
-            frames: {
-                max: 4,
-                hold: 60
-            },
-            animate: true,
-            isEnemy: true,
-            name:'Draggle',
-            attacks: [attacks.Tackle,attacks.Fireball ]
-        }
+        image: {
+            src: './img/embySprite.png'
+        },
+        frames: {
+            max: 4,
+            hold: 60
+        },
+        animate: true,
+        name: 'Emby',
+        attacks: [attacks.Tackle, attacks.Fireball]
+    },
+    Draggle: {
+        position: {
+            x: 1070,
+            y: 150,
+        },
+        image: {
+            src: './img/draggleSprite.png'
+        },
+        frames: {
+            max: 4,
+            hold: 60
+        },
+        animate: true,
+        isEnemy: true,
+        name: 'Draggle',
+        attacks: [attacks.Tackle, attacks.Fireball]
     }
+}
